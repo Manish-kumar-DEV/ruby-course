ff53d72e92b54deb0f62ad62ddb49bffcb513b5e
Manish Kumar <manish1771999@gmail.com>
2022-07-28 00:46:20 +0530

    add else statement

--- montys-color.rb
+++ montys-color.rb
@@ -6,14 +6,14 @@ color = gets.chop
 
 # Lesson 4: Using an If Statement
 
-if color == "blue"
-    puts "Wow! My favorite color is #{color} also!"
-end
+# if color == "blue"
+#     puts "Wow! My favorite color is #{color} also!"
+# end
 
 # Lesson 5: Adding an Else Statement
 
-# if color == "blue"
-#     puts "Wow! My favorite color is #{color} also!"
-# else
-#     puts "I'm not a big fan of #{color}."
-# end
+if color == "blue"
+    puts "Wow! My favorite color is #{color} also!"
+else
+    puts "I'm not a big fan of #{color}."
+end
