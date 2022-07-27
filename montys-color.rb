f4af54377e5bf2cefb65c360bff9efa57a71dc54
Manish Kumar <manish1771999@gmail.com>
2022-07-28 00:43:13 +0530

    starting with montys-color.rb

--- montys-color.rb
+++ montys-color.rb
@@ -1,8 +1,8 @@
 # Lesson 3: Prompt for Favorite Color
 
-# puts "What is your favorite color?"
-# color = gets.chop
-# puts "Wow! My favorite color is #{color} also!"
+puts "What is your favorite color?"
+color = gets.chop
+puts "Wow! My favorite color is #{color} also!"
 
 # Lesson 4: Using an If Statement
 
